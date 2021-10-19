365f535ad7fffc6c7c08466cc2d41421375caba3
Misza <misza@misza.net>
2021-10-19 23:43:40 +0200

    WinScreen autosizing

--- Scripts/WinScreen.gd
+++ Scripts/WinScreen.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 func _ready():
 	visible = false
+	var screen = get_viewport_rect().size
+	$LevelCompleteBackground.set_size(screen)
+	$LevelCompleteLabel.set_size(Vector2(screen.x, 100))
+	$LevelCompleteLabel.set_position(Vector2(0, screen.y / 2))
+	$LevelCompleteLabel.rect_pivot_offset.x = screen.x / 2
 
 func show():
 	$AnimationPlayer.play("show")
